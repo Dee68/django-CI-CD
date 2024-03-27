715084520b41dce8f0fe0ba24e0386ff158449d8
dimie egberipou <ddimie283@gmail.com>
2024-03-27 11:29:07 +0000

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def say_something():
+    return "<h1>What is wrong with leaders of today?</h1>"
+
+if __name__ == "__main__":
+    app.run()
